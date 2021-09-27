2febacbe02aef6a8a5560a07cbc1b2df886a57bd
Bashir <bashirokala@gmail.com>
2021-09-27 17:34:02 +0100

    initial commit

---
+++ src/test/java/com/sydani/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sydani.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
